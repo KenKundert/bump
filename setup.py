fde1bf5d477c5c858477e37e92492e12de2c42c2
Ken Kundert <ken@theKunderts.net>
2021-08-17 13:58:26 -0700

    enable zip_safe in installer

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup
 
 setup(
-    name='bump',
-    version='1.9.0',
-    author='Ken Kundert',
-    description='Manage version number and release date.',
-    license='GPLv3+',
-    scripts=['bump'],
-    install_requires=[
+    name = 'bump',
+    version = '1.9.0',
+    author = 'Ken Kundert',
+    description = 'Manage version number and release date.',
+    license = 'GPLv3+',
+    scripts = ['bump'],
+    install_requires = [
         'arrow',
         'docopt',
         'inform',
         'shlib',
     ],
+    zip_safe = True,
 )
